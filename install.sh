2d474b22f7a83d4c655eaa24fc83ceaafd3aab74
liumin <minliu2@cs.cmu.edu>
2024-09-06 17:10:10 -0400

    initial update

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pip install git+https://github.com/lucasb-eyer/pydensecrf.git
+pip install timm scikit-image pytorch_metric_learning fast_pytorch_kmeans opencv-python matplotlib
+mamba install faiss
